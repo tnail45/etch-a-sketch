10fd8fd92f1b569fa73c445e4d087bc4c8322c0c
tnail45 <tjnail2000@gmail.com>
2024-08-27 12:52:05 -0700

    Improve readability

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 const GRIDSIDE = 600;
-let rows = 16;
-let cols = 16;
+let numberOfSquares = 16;
 
 
 const sketchArea = document.querySelector("#sketch-area");
-sketchArea.style.width = `${GRIDSIDE}px`;
-sketchArea.style.height = `${GRIDSIDE}px`;
+sketchArea.style.width = sketchArea.style.height = `${GRIDSIDE}px`;
+
+function changeBackgroundColor() {
+    this.style.backgroundColor = "black";
+}
 
 function createGridCells() {
-    for (let i = 0; i < rows * cols; i++) {
+    for (let i = 0; i < (numberOfSquares * numberOfSquares); i++) {
         const gridCell = document.createElement("div");
 
-        gridCell.style.width = `${(GRIDSIDE / cols) - 2}px`;
-        gridCell.style.height = `${(GRIDSIDE / rows) - 2}px`;
-        gridCell.classList.add = ("cell");
+        gridCell.style.width = gridCell.style.height = `${(GRIDSIDE / numberOfSquares) - 2}px`;
+        gridCell.classList.add("cell");
 
         sketchArea.appendChild(gridCell);
+
+        gridCell.addEventListener("mouseover", changeBackgroundColor);
     }
 }
+
+createGridCells();
